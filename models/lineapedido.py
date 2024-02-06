e1a7799d5f749c2de23370ad46ec319775a2cb55
XoanOuteiro <xoanouteirojorge@gmail.com>
2024-02-06 14:18:34 +0100

    Creados forms search

--- models/lineapedido.py
+++ models/lineapedido.py
@@ -6,8 +6,10 @@ class lineapedido(models.Model):
 
     _name = 'odoo_basico.lineapedido'
     _description = 'exemplo lineapedido'
+    _order = "name desc"
 
     name = fields.Char(required=True, size=20, string="Nome:")
+    descripcion = fields.Text(string="Descripcion da linea de pedido")
     # Os campos Many2one crean un campo na BD
     pedido_id = fields.Many2one('odoo_basico.pedido',
                                 ondelete="cascade", required=True)
--- models/pedido.py
+++ models/pedido.py
@@ -6,9 +6,10 @@ class pedido(models.Model):
 
     _name = 'odoo_basico.pedido'
     _description = 'exemplo pedido'
+    _sql_constraints = [('nomeUnico','unique(name)','O identificador do pedido debe ser único')]
+    _order = 'name desc'
 
-    name = fields.Char(required=True, size=20, string="Nome:")
-    descripcion = fields.Text(string="A descripción:")
+    name = fields.Char(required=True, size=20, string="Identificador de pedido:")
 
     # Os campos One2many Non se almacenan na BD
     lineapedido_ids = fields.One2many("odoo_basico.lineapedido", 'pedido_id')
